dcf69c05661e7a9bdd7805ef31b33465ba5c5476
tndo <dotrann.1412@gmail.com>
2024-09-03 04:52:40 +0700

    updatmodule e link

--- cvprw2024_syntagen_teddybear/__init__.py
+++ cvprw2024_syntagen_teddybear/__init__.py
@@ -2,6 +2,9 @@ from .voc_classifier import VOCClassifier
 from synthlab_core.registry import register, ClassType
 from .voc_clipes import VOCCLIPES
 from .voc_classes import VOCClasses
+from . import _clipes_utilities
+from . import _clip
+from . import _pytorch_grad_cam
 
 register(ClassType.NODE, VOCClassifier)
 register(ClassType.NODE, VOCCLIPES)
