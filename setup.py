fdc473e6dc76c5a87f81d919fa5d36548b2b5ea7
Do Tran Ngoc <72861618+dotrann1412@users.noreply.github.com>
2024-09-11 14:36:20 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_urls = [
 ]
 
 dependencies = [
-    "synthlab_core @ git+https://github.com/synth-e/SynthLab-core",
+    "synthlab_core @ git+https://github.com/synth-e/synthlab-core",
     "gdown",
     "pydensecrf @ git+https://github.com/lucasb-eyer/pydensecrf",
     "lxml", 
